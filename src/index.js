afc75b639736fd58f489ec5a41fd308586df4cdd
Nelia <137152184+NeliaCou@users.noreply.github.com>
2023-09-25 10:28:47 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const secondCard = document.querySelector(".second-card");
+
+const secondCat = document.createElement("img");
+secondCat.src =
+  "https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_1280.jpg";
+
+secondCard.appendChild(secondCat);
